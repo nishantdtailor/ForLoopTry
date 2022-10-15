91b50d2fa1aed9a423a7fd63b806f68dede9c9d4
Nishant Tailor <nishantdtailor@gmail.com>
2022-10-15 17:30:29 +0100

    Version1_Commit

---
+++ src/FirstForLoop.java
@@ -0,0 +1,21 @@
+public class FirstForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10 ; i++) {
+
+            System.out.println(i);
+
+        }
+        System.out.println();
+
+        for (int j = 10; j >0 ; j--) {
+
+            System.out.println(j);
+
+        }
+
+
+    }
+
+}
