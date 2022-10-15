587cdaee4062f0eda5b25dcb68b0112da0fe9dfd
Nishant Tailor <nishantdtailor@gmail.com>
2022-10-15 22:37:54 +0100

    Version1_Commit

---
+++ src/Divby5.java
@@ -0,0 +1,13 @@
+public class Divby5
+{
+    public static void main(String[] args)
+    {
+        for (int i = 51; i <= 100; i++)
+        {
+            if(i%5==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
